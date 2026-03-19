f044d0f8ca0996719e40fdc5c643f73fa2f7ded8
Qasim <mianqasim065@gmail.com>
2026-03-20 02:20:49 +0500

    feat(models): implement user schema with pre-save password hashing

---
+++ src/models/user.model.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "name is required"],
+
+      trim: true,
+    },
+    email: {
+      type: String,
+      trim: true,
+      required: [true, "Email is required"],
+      unique: true,
+      lowercase: true,
+      match: [
+        /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+        "Please provide a valid email address",
+      ],
+    },
+    password: {
+      type: String,
+      required: true,
+      minlegnth: [6, "Password should be contain more than 6 characters"],
+      select: false, //password will not come implicitly with any query
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+userSchema.pre("save", async function () {
+  if (!this.isModified("password")) {
+    return;
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+const userModel = mongoose.model("user", userSchema);
+module.exports = userModel;
