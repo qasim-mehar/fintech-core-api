20390f597bcb8cdca2f84ca51000e6edc64647b4
Qasim <mianqasim065@gmail.com>
2026-03-25 00:55:40 +0500

    feat(routes): implement protected account creation endpoint

--- src/app.js
+++ src/app.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
 require("dotenv").config();
-const authRoute = require("./routes/auth.route");
 const app = express();
+
 app.use(express.json());
 app.use(cookieParser());
+/**
+ * - Routes require
+ */
+const authRoute = require("./routes/auth.route");
+const accountRoute = require("../src/routes/account.route");
 
+/**
+ * - use routes
+ */
 app.use("/api/auth", authRoute);
+app.use("/api/accounts", accountRoute);
 
 module.exports = app;
