aa568fb47e157059dbbe6ea51c0ae3f50ef457e9
Qasim <mianqasim065@gmail.com>
2026-03-26 17:59:55 +0500

    feat(auth): implement system user authorization middleware

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,6 +1,6 @@
 const jwt = require("jsonwebtoken");
 const userModel = require("../models/user.model");
-async function authMiddleware(req, res, next) {
+async function authUserMiddleware(req, res, next) {
   const token = req.cookies.token || req.header.authentication?.split(" ")[1];
   if (!token) {
     return res.status(401).json({
@@ -19,4 +19,29 @@ async function authMiddleware(req, res, next) {
     });
   }
 }
-module.exports = authMiddleware;
+async function authSystemUserMiddleware(req, res, next) {
+  const token = req.cookies.token || req.header.authorization.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({
+      message: "Unauthorized",
+    });
+  }
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const id = decoded.userId;
+    const user = await userModel.findById(id).select("+systemUser");
+    if (!user.systenUser) {
+      return res.status(403).json({
+        message: "Forbidden access, you are not a system user",
+      });
+    }
+
+    body.user = user;
+    next();
+  } catch (err) {
+    return res.status(401).json({
+      message: "Unauthorized",
+    });
+  }
+}
+module.exports = { authUserMiddleware, authSystemUserMiddleware };
