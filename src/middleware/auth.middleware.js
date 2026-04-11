9846d203e1600d26a291547f3815bf2417782c0c
Qasim <mianqasim065@gmail.com>
2026-04-11 11:46:36 +0500

    feat(auth): check token blacklisting in auth middlewares

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,11 +1,20 @@
 const jwt = require("jsonwebtoken");
 const userModel = require("../models/user.model");
 const accountModel = require("../models/account.model");
+const tokenBlacklistModel = require("../models/blackList.model");
 async function authUserMiddleware(req, res, next) {
   const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
   if (!token) {
     return res.status(401).json({
       message: "unauthorized",
+    });
+  }
+  const isTokenBlacklisted = await tokenBlacklistModel.findOne({
+    token,
+  });
+  if (isTokenBlacklisted) {
+    return res.status(400).json({
+      message: "Unauthorized access, token is invalid",
     });
   }
   try {
@@ -23,11 +32,22 @@ async function authUserMiddleware(req, res, next) {
 }
 async function authSystemUserMiddleware(req, res, next) {
   const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+
   if (!token) {
     return res.status(401).json({
       message: "Unauthorized",
     });
   }
+
+  const isTokenBlacklisted = await tokenBlacklistModel.findOne({
+    token,
+  });
+  if (isTokenBlacklisted) {
+    return res.status(400).json({
+      message: "Unauthorized access, token is invalid",
+    });
+  }
+
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     const id = decoded.userId;
